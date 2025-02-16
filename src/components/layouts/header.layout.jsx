13fffe0e233de94b2c6dc80fbb3390233681c3cd
rounakkhuntia <khuntiarounak@gmail.com>
2025-02-16 19:45:03 +0530

    added home hero section and trending destination layout

--- src/components/layouts/header.layout.jsx
+++ src/components/layouts/header.layout.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Button } from '../ui/button'
+import { Button } from '@/components/ui/button';
+import { SERVICE_LIST } from '@/config/app.config';
+import Icon from '../ui/icon';
 
 const Header = () => {
   return (
@@ -15,11 +16,20 @@ const Header = () => {
                 />
                 </a>
             </div>
-            <div id="auth" className="flex gap-2 justify-center items-center">
-                <Button className="bg-white cursor-pointer border-primary text-primary rounded-sm hover:bg-white/95">Register</Button>
-                <Button className="bg-white cursor-pointer border-primary text-primary rounded-sm hover:bg-white/95">Login</Button>
-            </div>
         </div>
+        <div id="auth" className="flex gap-2 justify-center items-center">
+            <Button className="bg-white cursor-pointer border-primary text-primary rounded-sm hover:bg-white/95">Register</Button>
+            <Button className="bg-white cursor-pointer border-primary text-primary rounded-sm hover:bg-white/95">Login</Button>
+            <div className='container flex gap-1'>
+                {SERVICE_LIST.map(item => (
+                    <Button key={item.id} className={`bg-transparent shadow-none font-normal rounded-full hover:bg-white/10
+                    cursor-pointer flex items-center justify-between gap-2 px-6 h-11 ${item.active && 'border border-white bg-white/10'}`}>
+                    <Icon icon={item.icon} />
+                    {item.title}
+                    </Button>
+                ))}
+        </div>
+    </div>
     </header>
   )
 }
